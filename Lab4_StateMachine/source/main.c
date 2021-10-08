b4b55de84d96dffe27129557344013fa552e4250
Yiu Ming Wong <dexteryiumingwong@gmail.com>
2021-10-08 16:30:25 -0700

    save work

--- Lab4_StateMachine/source/main.c
+++ Lab4_StateMachine/source/main.c
@@ -1,7 +1,7 @@
-/*	Author: lab
+/*	Author: Yiu Ming Wong
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Lab Section: 021
+ *	Assignment: Lab #4  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -12,12 +12,74 @@
 #include "simAVRHeader.h"
 #endif
 
+enum States {Start,On_Press,On_Release,Off_Press,Off_Release}state;
+void Tick(){
+	unsigned char tmpA=PINA&0x01;
+	switch(state){
+		case Start:
+			state=On_Press;
+			break;
+		case On_Press:
+			if(tmpA==0x01){
+				state =On_Press;
+			}else{
+				state = On_Release;
+			}
+			break;
+		case On_Release:
+			if(tmpA==0x01){
+				state = Off_Press;
+			}else{
+				state = On_Release;
+			}
+			break;
+		case Off_Press:
+			if(tmpA==0x01){
+				state = Off_Press;
+			}
+			else{
+				state = Off_Release;
+			}
+			break;
+		case Off_Release:
+			if(tmpA==0x01){
+				state = On_Press;
+			}
+			else{
+				state = Off_Release;
+			}
+			break;
+				
+		default:
+			break;
+	}
+	switch(state){
+		case Start:
+			PORTB=0x01;
+			break;
+		case On_Press:
+			PORTB=0x01;
+			break;
+		case On_Release:
+			break;
+		case Off_Press:
+			PORTB=0x02;
+			break;
+		case Off_Release:
+			break;
+		default:
+			break;
+	}
+}
+
+
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+	DDRA= 0x00; PORTA=0xFF;
+	DDRB= 0xFF; PORTB= 0x00;
     /* Insert your solution below */
     while (1) {
-
+	Tick();
     }
     return 1;
 }
--- Lab4_StateMachine/test/tests.gdb
+++ Lab4_StateMachine/test/tests.gdb
@@ -27,18 +27,26 @@ echo ======================================================\n
 echo Running all tests..."\n\n
 
 # Example test:
-test "PINA: 0x00, PINB: 0x00 => PORTC: 0"
+#test "PINA: 0x00, PINB: 0x00 => PORTC: 0"
 # Set inputs
-setPINA 0x00
-setPINB 0x00
+#setPINA 0x00
+#setPINB 0x00
 # Continue for several ticks
-continue 2
+#continue 2
 # Set expect values
-expectPORTC 0
+#expectPORTC 0
 # Check pass/fail
-checkResult
+#checkResult
 
 # Add tests below
+test(1) "Testing button press once"
+set state = Start;
+setPINA 0x01
+continue 5;
+expectPORTB 0x02;
+checkResults
+
+
 
 # Report on how many tests passed/tests ran
 set $passed=$tests-$failed
