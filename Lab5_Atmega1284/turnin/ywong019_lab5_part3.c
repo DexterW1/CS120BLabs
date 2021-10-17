ef5923965e72a23823513ba359b8cd77b9fc459c
Yiu Ming Wong <dexteryiumingwong@gmail.com>
2021-10-17 13:00:20 -0700

    Completed Part 3

---
+++ Lab5_Atmega1284/turnin/ywong019_lab5_part3.c
@@ -0,0 +1,88 @@
+
+/*      Author: Yiu Ming Wong
+ *  Partner(s) Name: 
+ *      Lab Section: 021
+ *      Assignment: Lab #5  Exercise #2
+ *      Exercise Description: [optional - include for your own benefit]
+ *
+ *      I acknowledge all content contained herein, excluding template or example
+ *      code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+unsigned arr[12]={0x00,0x01,0x03,0x07,0x0F,0x1F,0x3F,0x21,0x12,0x0C,0x15,0x2A};
+unsigned char counter =0x00;
+enum States {Start,Begin,InitialValues,Plus,Plus_led,Reset}state;
+void Tick(){
+        unsigned char tmpA=~PINA&0x01;
+        switch(state){
+                case Start:
+                        state=Begin;
+                        break;
+		case Begin:
+			state = InitialValues;
+                case InitialValues:
+			if(counter>12){
+				state = Reset;
+			}
+			else if(tmpA==0x01){
+				state = Plus_led;
+			}
+			break;
+		case Reset:
+			state = InitialValues;	
+			break;
+		case Plus:
+			if(tmpA==0x01){
+				state = Plus;
+			}
+			
+			else{
+				state = InitialValues;
+			}
+			break;
+		case Plus_led:
+			state = Plus;
+			break;
+                default:
+			state = Start;
+                        break;
+        }
+        switch(state){
+                case Start:
+                        break;
+		case Begin:
+			PORTC=0x00;
+                case InitialValues:
+                        break;
+                case Reset:
+			PORTC= 0x00;
+			counter=0x00;
+                        break;
+		case Plus:
+			break;
+		case Plus_led:
+			PORTC=arr[counter];
+			counter++;
+            		break;
+                default:
+		
+                        break;
+        }
+}
+
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+        DDRA= 0x00; PORTA=0xFF;
+        DDRC= 0xFF; PORTC= 0x00;
+    /* Insert your solution below */
+    while (1) {
+        Tick();
+    }
+    return 1;
+}
+
